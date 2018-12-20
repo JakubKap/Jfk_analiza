5eeac6729af5b564abd15aaa9b0b630c046e73ef
Jakub Kapusta <jakubkapusta39@gmail.com>
2018-12-20 20:52:15 +0100

    Displaying proper type of an error connected with sqtr function

--- frac.py
+++ frac.py
@@ -122,7 +122,7 @@ def mfmax(args):
     return fmax(args).negative()
 
 def fsqrt(a):
-    assert (a.nom >= 0), "argumentem funkcji jest liczba ujemna"
+    assert (a.nom >= 0), "argumentem funkcji sqrt jest liczba ujemna"
     nomres = sqrt(a.nom)
     denres = sqrt(a.den)
 
